da78ea7cc79997cddb2877f03c4fea7f4ea0bc10
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2016-11-28 19:45:45 +0100

    [master] production test

--- resources/views/frontend/navbar.blade.php
+++ resources/views/frontend/navbar.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{!! route('home') !!}">Staging</a>
+            <a class="navbar-brand" href="{!! route('home') !!}">Production</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
