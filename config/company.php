376c25b23e68cc31608391125be4db7d9f6c1c68
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2016-12-06 13:18:58 +0100

    [staging] address

--- config/company.php
+++ config/company.php
@@ -5,6 +5,6 @@ return [
     'email' => 'info@seksvriend.nl',
     'kvk' => '64194140',
     'btw' => 'NL206692304B01',
-    'Adres' => 'De Ganskuijl 73',
+    'address' => 'De Ganskuijl 73',
     'city' => 'Amersfoort',
 ];
