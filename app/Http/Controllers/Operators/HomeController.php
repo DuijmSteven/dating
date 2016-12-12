b813e52139fe218423101e8a90c4bcf1dec43bb5
apmavrid <apmavrid@gmail.com>
2016-12-12 23:16:45 +0200

    [staging] new messages functionality

--- app/Http/Controllers/Operators/HomeController.php
+++ app/Http/Controllers/Operators/HomeController.php
@@ -20,22 +20,51 @@ class HomeController extends \App\Http\Controllers\Controller
                             ->havingRaw('count(distinct sender_id) < 2')
                             ->get();
 
-        $newConversations = $this->getNewConversations($newConversationsIds);
+        $newConversationsIdsArray = $this->getConversationsOrArrayOfIds($newConversationsIds, 1);
+
+        $conversationsWithNewMessagesIds = \DB::table('conversation_messages')
+                        ->join('role_user', function ($join) {
+                            $join->on('conversation_messages.sender_id', '=', 'role_user.user_id')
+                                 ->where('role_user.role_id', '=', 2);
+                        })
+                        ->select('conversation_id')
+                        ->whereNotIn('conversation_id', $newConversationsIdsArray)
+                        ->orderBy('conversation_messages.created_at', 'desc')
+                        ->take(1)
+                        ->get();
+
+        $newConversations = $this->getConversationsOrArrayOfIds($newConversationsIds);
+
+        $conversationsWithNewMessages = $this->getConversationsOrArrayOfIds($conversationsWithNewMessagesIds);
 
         return view(
             'operators.dashboard',
             [
-                'newConversations' => $newConversations
+                'newConversations' => $newConversations,
+                'conversationsWithNewMessages' => $conversationsWithNewMessages
             ]
         );
     }
 
-    public function getNewConversations($newConversationsIds)
+    /**
+     * @param $Ids
+     * @param int $getArrayOfIds
+     * @return array|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null|static|static[]
+     */
+    public function getConversationsOrArrayOfIds($Ids, int $getArrayOfIds = 0)
     {
-        foreach ($newConversationsIds as $id) {
-            $allIds[] = $id->conversation_id;
+        if ($Ids->count()) {
+            foreach ($Ids as $id) {
+                $allIds[] = $id->conversation_id;
+            }
+        } else {
+            return [];
         }
 
-        return Conversation::with(['messages', 'userA', 'userB'])->find($allIds);
+        if ($getArrayOfIds) {
+            return $allIds;
+        } else {
+            return Conversation::with(['messages', 'userA', 'userB'])->find($allIds);
+        }
     }
 }
--- resources/views/operators/dashboard.blade.php
+++ resources/views/operators/dashboard.blade.php
@@ -58,13 +58,47 @@
             <div class="panel-heading" role="tab" id="headingTwo">
                 <h4 class="panel-title">
                     <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Flirts <span class="label label-success">4</span>
+                        New Messages <span class="label label-success">{!! count($conversationsWithNewMessages) !!}</span>
                     </a>
                 </h4>
             </div>
             <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
                 <div class="panel-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <div class="row">
+                        @foreach($conversationsWithNewMessages as $conversation)
+                            @php
+                                $message = $conversation->messages->last();
+
+                                if ($message->sender_id === $conversation->userA->id) {
+                                    $user = $conversation->userA;
+                                } else {
+                                    $user = $conversation->userB;
+                                }
+                            @endphp
+                            <div class="col-xs-12 col-sm-4 col-md-3">
+                                <!-- Widget: user widget style 1 -->
+                                <div class="box box-widget widget-user-2 default-border">
+                                    <!-- Add the bg color to the header using any of the bg-* classes -->
+                                    <div class="convo-tile">
+                                        <img src="http://placehold.it/70x50">
+                                        <div class="convo-tile_text">
+                                            <div class="username">{!! $user->username !!}</div>
+                                            <div class="date">{!! $message->created_at !!}</div>
+                                        </div>
+                                        <!-- /.widget-user-image -->
+                                    </div>
+                                    <div class="box-footer no-padding">
+                                        <div class="text-summary">
+                                            {!! $message->body !!}
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <a href="conversations/{!! $conversation->id !!}" class="btn btn-primary btn-flat btn-block">View Conversation</a>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
@@ -72,13 +106,13 @@
             <div class="panel-heading" role="tab" id="headingThree">
                 <h4 class="panel-title">
                     <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Gifts <span class="label label-success">4</span>
+                        Flirts <span class="label label-success">4</span>
                     </a>
                 </h4>
             </div>
             <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
                 <div class="panel-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
                 </div>
             </div>
         </div>
