383bb05c9c60e5a1e30b6836966b6d07bccb5c9a
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2017-02-27 23:43:03 +0100

    [staging] small stuff and cleanup

--- resources/views/backend/layouts/default/partials/header.blade.php
+++ resources/views/backend/layouts/default/partials/header.blade.php
@@ -237,25 +237,21 @@
                         <!-- Menu Body -->
                         <li class="user-body">
                             <div class="row">
-                                <div class="col-xs-4 text-center">
-                                    <a href="#">Followers</a>
-                                </div>
-                                <div class="col-xs-4 text-center">
-                                    <a href="#">Sales</a>
-                                </div>
-                                <div class="col-xs-4 text-center">
-                                    <a href="#">Friends</a>
-                                </div>
                             </div>
                             <!-- /.row -->
                         </li>
                         <!-- Menu Footer-->
                         <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            </div>
                             <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                <a  href="{!!  route('logout.post') !!}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();"
+                                    class="btn btn-default btn-flat">
+                                    Log out
+                                </a>
+                                <form id="logout-form" action="{!!  route('logout.post') !!}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
                             </div>
                         </li>
                     </ul>
