d22961c440fb694af1e65fe495ac6d45e767b257
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2017-07-02 02:21:57 +0200

    [develop] getters and setters

--- app/Article.php
+++ app/Article.php
@@ -21,4 +21,68 @@ class Article extends Model
         0 => 'private',
         1 => 'public'
     ];
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     */
+    public function setBody(string $body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMetaDescription()
+    {
+        return $this->meta_description;
+    }
+
+    /**
+     * @param string $metaDescription
+     */
+    public function setMetaDescription(string $metaDescription)
+    {
+        $this->meta_description = $metaDescription;
+    }
 }
