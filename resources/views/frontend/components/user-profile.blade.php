6265f78a13601b4544c0842c8582e6525e45696d
apmavrid <apmavrid@gmail.com>
2017-07-18 16:04:59 +0300

    [develop] user profile changes

--- resources/views/frontend/components/user-profile.blade.php
+++ resources/views/frontend/components/user-profile.blade.php
@@ -22,10 +22,51 @@
                 </div>
             </div>
             <div class="col-xs-12 col-sm-9">
-                <h5>About me</h5>
+                <h5><i class="fa fa-user"></i> Information</h5>
+                <hr>
+                <div class="row User-profile__text">
+                    <div class="col-xs-6">
+                        <div> <strong>Age:</strong> {{ $user->meta->dob->diffInYears($carbonNow) }}</div>
+                        <div> <strong>Status:</strong>
+                            {{ ucfirst(str_replace('_', ' ', \UserConstants::selectableField('relationship_status', $user->roles[0]->name)[$user->meta->relationship_status])) }}
+                        </div>
+                        <div> <strong>Province:</strong>
+                            {{ ucfirst(str_replace('_', ' ', \UserConstants::selectableField('province', $user->roles[0]->name)[$user->meta->relationship_status])) }}
+                        </div>
+                        <div> <strong>City:</strong>
+                            {{ $user->meta->city }}
+                        </div>
+                        <div> <strong>Height:</strong>
+                            {{ ucfirst(str_replace('_', ' ', \UserConstants::selectableField('height', $user->roles[0]->name)[$user->meta->relationship_status])) }}
+                        </div>
+                    </div>
+                    <div class="col-xs-6">
+                        <div> <strong>Body type:</strong>
+                            {{ ucfirst(str_replace('_', ' ', \UserConstants::selectableField('body_type', $user->roles[0]->name)[$user->meta->relationship_status])) }}
+                        </div>
+                        <div> <strong>Eye color:</strong>
+                            {{ ucfirst(str_replace('_', ' ', \UserConstants::selectableField('eye_color', $user->roles[0]->name)[$user->meta->relationship_status])) }}
+                        </div>
+                        <div> <strong>Hair color:</strong>
+                            {{ ucfirst(str_replace('_', ' ', \UserConstants::selectableField('hair_color', $user->roles[0]->name)[$user->meta->relationship_status])) }}
+                        </div>
+                        <div> <strong>Smoking:</strong>
+                            {{ ucfirst(str_replace('_', ' ', \UserConstants::selectableField('smoking_habits', $user->roles[0]->name)[$user->meta->relationship_status])) }}
+                        </div>
+                        <div> <strong>Drinking:</strong>
+                            {{ ucfirst(str_replace('_', ' ', \UserConstants::selectableField('drinking_habits', $user->roles[0]->name)[$user->meta->relationship_status])) }}
+                        </div>
+                    </div>
+                </div>
+                <h5><i class="fa fa-book"></i> About me</h5>
                 <hr>
                 <div class="User-profile__text">
                     {{ $user->meta->about_me }}
+                </div>
+                <h5><i class="fa fa-search"></i> Looking for</h5>
+                <hr>
+                <div class="User-profile__text">
+                    {{ $user->meta->looking_for }}
                 </div>
             </div>
         </div>
