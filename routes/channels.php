0bae66f8b044993d409047a7375d773416777788
apmavrid <apmavrid@gmail.com>
2018-02-23 23:23:33 +0200

    [develop] chat privacy changes

--- routes/channels.php
+++ routes/channels.php
@@ -15,7 +15,18 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('chat', function ($user) {
-    return Auth::check();
+Broadcast::channel('chat.{conversationId}', function ($user, $conversationId) {
+    if (
+        ($user->id == \App\ConversationMessage::find($conversationId, ['conversation_id'])->first()->sender_id)
+        ||
+        ($user->id == \App\ConversationMessage::find($conversationId, ['conversation_id'])->first()->recipient_id)
+    ) {
+        return true;
+    }
+    return false;
 });
+
+/*Broadcast::channel('chat', function ($user) {
+    return Auth::check();
+});*/
 
