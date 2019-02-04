dc508f6fa8ca746bd5b2dc46c1a88e6e6e75b98c
apmavrid <apmavrid@gmail.com>
2019-02-05 00:00:16 +0200

    article view changes

--- resources/views/frontend/articles/overview.blade.php
+++ resources/views/frontend/articles/overview.blade.php
@@ -8,12 +8,28 @@
     </div>
     <div class="Tile__body Tile__article">
         @foreach ($articles as $article)
-            <div>
-                <a href="{{ route('articles.show', ['articleId' => $article->getId()])  }}">{{ $article->title }}</a>
+            <div class="row">
+                <div class="col-md-3 text-center img-responsive">
+                    <img src="http://placehold.it/250x250">
+                </div>
+                <div class="col-md-9">
+                    <div>
+                        <a href="{{ route('articles.show', ['articleId' => $article->getId()])  }}">{{ strtoupper($article->title) }}</a>
+                    </div>
+                    <div class="margin10 fontSize15">
+                        <i>Posted on {{ $article->created_at->toFormattedDateString() }}</i>
+                    </div>
+                    <div>
+                        {{ str_limit($article->getBody(), 400) }}
+                    </div>
+                    <div class="text-right margin10">
+                        <button type="submit" class="Button Button--primary">
+                            <span class="Button__content">Lees Meer</span>
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div>
-                {{ $article->getMetaDescription() }}
-            </div>
+
             <hr>
         @endforeach
     </div>
