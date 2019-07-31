d9197e57990a85f71c25862a1517c153f9600ae3
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2019-07-31 14:39:33 +0200

    add email_types table and email_type_user table and models

---
+++ app/EmailType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailType extends Model
+{
+    public $table = 'email_types';
+
+}
---
+++ database/migrations/2019_07_31_105517_create_email_type_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailTypeUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_type_user', function (Blueprint $table) {
+            $table->integer('email_type_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->boolean('active');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_type_user');
+    }
+}
