a9bd0d9151ed519de1c53a7c00278871e1cc379a
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2019-12-18 13:07:06 +0100

    mysql table backups

---
+++ resources/backups/mysql/dating_roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO dating.roles (id, name) VALUES (1, 'admin');
+INSERT INTO dating.roles (id, name) VALUES (2, 'peasant');
+INSERT INTO dating.roles (id, name) VALUES (3, 'bot');
+INSERT INTO dating.roles (id, name) VALUES (4, 'operator');
