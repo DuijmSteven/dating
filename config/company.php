cd7d73552780a47bd4921407f4dfd4162bada3df
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-01-27 18:51:24 +0100

    company data

--- config/company.php
+++ config/company.php
@@ -3,7 +3,7 @@
 return [
     'name' => 'Devely V.O.F.',
     'info_email' => 'info@altijdsex.nl',
-    'post_code' => 'info@seksvriend.nl',
+    'post_code' => '1316 BZ',
     'kvk' => '77128036',
     'btw' => 'NL860909979B01',
     'address' => 'Randstad 22 42',
