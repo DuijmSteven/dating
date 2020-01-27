0d7e3b1c8fd31236f02cbb3c762469cabaf0e885
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-01-27 18:45:28 +0100

    company data

--- config/company.php
+++ config/company.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'name' => 'Dubble Digital',
+    'name' => 'Devely V.O.F.',
     'info_email' => 'info@altijdsex.nl',
     'post_code' => 'info@seksvriend.nl',
-    'kvk' => '64194140',
-    'btw' => 'NL206692304B01',
-    'address' => 'De Ganskuijl 73',
-    'city' => 'Amersfoort',
+    'kvk' => '77128036',
+    'btw' => 'NL860909979B01',
+    'address' => 'Randstad 22 42',
+    'city' => 'Almere',
 ];
