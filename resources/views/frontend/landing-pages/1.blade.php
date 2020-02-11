b6f97196628d1c02c52e4cb00bafb595aa579a63
apmavrid <apmavrid@gmail.com>
2020-02-11 11:50:56 +0200

    small seo related changes

--- resources/views/frontend/landing-pages/1.blade.php
+++ resources/views/frontend/landing-pages/1.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="nl">
 @include('frontend.layouts.default.partials.head')
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -418,7 +418,7 @@
 <header>
     <div class="container">
         <div class="navbar-brand">
-            <img src="img/site_logos/Altijdsex_LogoBig_Pos.svg"/>
+            <img src="img/site_logos/Altijdsex_LogoBig_Pos.svg" title="Altijdsex.nl Logo" alt="Altijdsex.nl Logo"/>
         </div>
     </div>
 </header>
