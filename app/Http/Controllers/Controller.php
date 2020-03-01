2d39c5c8600ac2cd8b321846a936e99ee3e70908
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-03-01 20:28:31 +0100

    fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -28,7 +28,7 @@ class Controller extends BaseController
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
-            $this->authenticatedUser = UserManager::getAndFormatAuthenticatedUser2();
+            $this->authenticatedUser = UserManager::getAndFormatAuthenticatedUser();
 
             $onlineIds = Activity::users(9)->pluck('user_id')->toArray();
 
