75cb30f958afd3e2600694f19b44b0a85c48bc47
apmavrid <apmavrid@gmail.com>
2020-03-06 22:54:29 +0200

    font-awesome removed

--- resources/views/frontend/thank-you.blade.php
+++ resources/views/frontend/thank-you.blade.php
@@ -1,13 +1,11 @@
 @extends('frontend.layouts.default.layout')
 
 @section('content')
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
     <div class="row">
         <div class="col-md-12 text-center" style="margin: 30% 20px 30% 20px">
             @if($status == true)
                 <h1 id="analytics--successfulPayment" style="font-size: 60px; font-weight: 700;">{{ @trans('credits_bought_feedback.thanks') }}</h1>
-                <i class="fa fa-check" style="font-size: 10rem; color: #2ab27b"></i>
+                <i class="material-icons" style="font-size: 10rem; color: #2ab27b">done</i>
                 <h4>{{ @trans('credits_bought_feedback.success_message') }}</h4>
                 <a href="{{ route('home') }}" class="btn btn-success btn-lg btn-block" style="margin-top: 30px">
                     {{ @trans('credits_bought_feedback.home') }}
