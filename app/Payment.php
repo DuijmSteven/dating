5b3587b0aca98a1d666027f8b84d4f87af884b57
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-03-19 18:29:42 +0100

    fix job

--- app/Payment.php
+++ app/Payment.php
@@ -28,7 +28,7 @@ class Payment extends TimeZonedModel
 
     public function creditpack()
     {
-        return $this->hasOne(Creditpack::class);
+        return $this->belongsTo(Creditpack::class, 'creditpack_id', 'id');
     }
 
     public function getDescription()
