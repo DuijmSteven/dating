5c7de58898020eaaae09a9ba0b5945704e9fa0af
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-03-29 18:02:42 +0200

    logs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -47,7 +47,7 @@ class Kernel extends ConsoleKernel
         }
         
         if (config('app.env') === 'staging') {
-            $schedule->command(UpdateCurrentEnvDbAndAws::class)->dailyAt("05:30");
+            $schedule->command(UpdateCurrentEnvDbAndAws::class)->dailyAt("17:05");
         }
     }
 
