471284a9a3858c9ff5fc22ef0de78d683b0b43bf
apmavrid <apmavrid@gmail.com>
2020-04-02 20:29:19 +0300

    New method to get client IP

--- app/Traits/Users/RegistersUsers.php
+++ app/Traits/Users/RegistersUsers.php
@@ -213,7 +213,7 @@ trait RegistersUsers
         if ($user->affiliateTracking()->exists()) {
             $genderLookingForGender = explode("-", $request->all()['lookingFor']);
             $gender = $genderLookingForGender[0];
-            $clientIP = \Request::ip();
+            $clientIP = $this->getUserIp();
             $countryCode = $this->getLocationFromIp($clientIP);
             \Log::debug('Country code: ' . $countryCode);
             \Log::debug('IP: ' . $clientIP);
@@ -254,5 +254,22 @@ trait RegistersUsers
                 \Log::error('Cannot get IP - ' . Psr7\str($e->getResponse()));
             }
         }
+    }
+
+    protected function getUserIp()
+    {
+        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+            $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        }
+        $client  = @$_SERVER['HTTP_CLIENT_IP'];
+        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $remote  = $_SERVER['REMOTE_ADDR'];
+
+        if(filter_var($client, FILTER_VALIDATE_IP)) { $ip = $client; }
+        elseif(filter_var($forward, FILTER_VALIDATE_IP)) { $ip = $forward; }
+        else { $ip = $remote; }
+
+        return $ip;
     }
 }
