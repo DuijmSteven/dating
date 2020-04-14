1f2509696e070cbb0362df1836e4c3a4f07379ab
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-04-14 20:42:26 +0200

    remove new convos from unreplied category

--- app/Managers/ConversationManager.php
+++ app/Managers/ConversationManager.php
@@ -258,7 +258,7 @@ class ConversationManager
             ->with(['messages' => function ($query) {
                 $query->orderBy('created_at', 'desc');
             }])
-            ->has('messages', '>', '0')
+            ->has('messages', '>', '1')
             ->where(function ($query) {
                 $query->where('locked_at', null)
                     ->orWhere('locked_at', '<', Carbon::now()->subMinutes(self::CONVERSATION_LOCKING_TIME));
