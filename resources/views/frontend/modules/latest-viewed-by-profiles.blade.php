25b6d03fd6ff9091cbee856c265d37eb3384f79f
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-04-23 17:50:23 +0200

    fix problem with missing users

--- resources/views/frontend/modules/latest-viewed-by-profiles.blade.php
+++ resources/views/frontend/modules/latest-viewed-by-profiles.blade.php
@@ -8,20 +8,22 @@
     </div>
     <div class="Tile__body LatestViewedBy__body">
         @forelse($latestViewedBy as $view)
-            <a href="{!! route('users.show', ['username' => $view->viewer->getUsername()]) !!}" class="LatestViewedBy__item">
-                <span class="LatestViewedBy__profile-image">
-                    <img src="{!! \StorageHelper::profileImageUrl($view->viewer, true) !!}" alt="">
+            @if(null != $view->viewer)
+                <a href="{!! route('users.show', ['username' => $view->viewer->getUsername()]) !!}" class="LatestViewedBy__item">
+                    <span class="LatestViewedBy__profile-image">
+                        <img src="{!! \StorageHelper::profileImageUrl($view->viewer, true) !!}" alt="">
 
-                    @if(in_array($view->viewer->getId(), $onlineUserIds))
-                        <div class="onlineCircle"></div>
-                    @endif
-                </span>
+                        @if(in_array($view->viewer->getId(), $onlineUserIds))
+                            <div class="onlineCircle"></div>
+                        @endif
+                    </span>
 
-                <span class="LatestViewedBy__username">
-                    {{ $view->viewer->username }}{{ $view->viewer->meta->dob ? ', ' : '' }}
-                    {{ $view->viewer->meta->dob ? $view->viewer->meta->dob->diffInYears(\Carbon\Carbon::now('Europe/Amsterdam')) : '' }}
-                </span>
-            </a>
+                    <span class="LatestViewedBy__username">
+                        {{ $view->viewer->username }}{{ $view->viewer->meta->dob ? ', ' : '' }}
+                        {{ $view->viewer->meta->dob ? $view->viewer->meta->dob->diffInYears(\Carbon\Carbon::now('Europe/Amsterdam')) : '' }}
+                    </span>
+                </a>
+            @endif
         @empty
             <p>
                 {{ trans('viewed_by_users.no_views') }}
