dc0c4c19d37a4457eb44afbf3d1c62fa15d80e02
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-06-02 02:17:21 +0200

    typo

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -169,7 +169,7 @@
             </div>
         </div>
 
-G        <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="col-xs-12 col-sm-6 col-md-4">
             <div class="box box-widget DashboardWidget">
                 <!-- Add the bg color to the header using any of the bg-* classes -->
                 <div class="bg-primary">
