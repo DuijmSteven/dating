5553316878e597ccdd6e789336d5a69f654c9399
apmavrid <apmavrid@gmail.com>
2020-06-06 15:36:37 +0300

    Notifications body initial design

--- resources/assets/js/vue/components/notifications/NotificationsManager.vue
+++ resources/assets/js/vue/components/notifications/NotificationsManager.vue
@@ -1,11 +1,38 @@
 <template>
     <div class="NotificationsManager">
-        <div class="NotificationsManager__head">
+        <div v-on:click="toggle"
+             class="NotificationsManager__head"
+        >
             <div class="NotificationsManager__toggle">
+                <div
+                    class="NotificationsManager__toggle__text"
+                    style="display: none"
+                >
+                    <span>Notifications (3)</span>
+
+                    <i class="NotificationsManager__toggle__minimize material-icons"
+                    >
+                        minimize
+                    </i>
+                </div>
+                <div class="NotificationsManager__toggle__icons">
+                    <i class="material-icons">
+                        notifications
+                    </i>
+                </div>
+                <span class="NotificationsManager__toggle__icons NotificationsManager__newNotifications">3</span>
+            </div>
+        </div>
+        <div
+            id="NotificationsManager__body"
+            class="NotificationsManager__body"
+            style="display: none"
+        >
+            <div class="NotificationsManager__body__item">
+                <span>Jane viewed your profile</span>
                 <i class="material-icons">
-                    notifications
+                    close
                 </i>
-                <span class="NotificationsManager__newNotifications">3</span>
             </div>
         </div>
     </div>
@@ -13,7 +40,15 @@
 
 <script>
     export default {
-        name: "NotificationsManager"
+        name: "NotificationsManager",
+        methods: {
+            toggle: function () {
+                $('#NotificationsManager__body').slideToggle('fast');
+                $('.NotificationsManager').toggleClass('maximized')
+                $('.NotificationsManager__toggle__text').toggle();
+                $('.NotificationsManager__toggle__icons').toggle();
+            }
+        }
     }
 </script>
 
