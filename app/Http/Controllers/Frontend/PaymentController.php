411f4c84bc5b85d8acb34109ac27689a1a361d5d
apmavrid <apmavrid@gmail.com>
2020-06-14 10:12:09 +0300

    Call xpartners API for other publishers

--- app/Http/Controllers/Frontend/PaymentController.php
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -201,7 +201,11 @@ class PaymentController extends FrontendController
             ->queue($userBoughtCreditsEmail);
 
         //In case the buyer came from an affiliate, hit publisher callback
-        /*if ($user->affiliateTracking()->exists()) {
+        if (
+            $user->affiliateTracking()->exists() &&
+            $user->affiliateTracking->getAffiliate() == 'xpartners' &&
+            $user->affiliateTracking->getMediaId() != '147374'
+        ) {
             $client = new Client();
             try {
                 $response = $client->request(
@@ -217,6 +221,6 @@ class PaymentController extends FrontendController
                     \Log::error('Affiliate postback error - '.Psr7\str($e->getResponse()));
                 }
             }
-        }*/
+        }
     }
 }
