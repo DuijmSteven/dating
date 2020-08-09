e688937ac43c53fa3e7e4aa85af25abe8c032e67
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-08-09 21:08:20 +0200

    fix country in registrations

--- resources/views/frontend/landing-pages/1.blade.php
+++ resources/views/frontend/landing-pages/1.blade.php
@@ -775,6 +775,7 @@
                             @if(isset($clickId))
                                 <input type="hidden" name="clickId" value="{{ $clickId }}">
                                 <input type="hidden" name="affiliate" value="{{ $affiliate }}">
+                                <input type="hidden" name="country" value="{{ $country }}">
                             @elseif(isset($affiliate) && $affiliate === \App\UserAffiliateTracking::AFFILIATE_DATECENTRALE)
                                 <input type="hidden" name="affiliate" value="{{ \App\UserAffiliateTracking::AFFILIATE_DATECENTRALE }}">
                             @endif
