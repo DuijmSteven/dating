650e5026bf39ae93e2eea708fc912eaf8777d722
Orestis Palampougioukis <orestis.palampougioukis@gmail.com>
2020-09-21 19:31:38 +0200

    improve home loading speed

--- database/migrations/2020_09_21_171450_add_indices_to_user_meta_table.php
+++ database/migrations/2020_09_21_171450_add_indices_to_user_meta_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIndexToUserMetaTable extends Migration
+class AddIndicesToUserMetaTable extends Migration
 {
     /**
      * Run the migrations.
