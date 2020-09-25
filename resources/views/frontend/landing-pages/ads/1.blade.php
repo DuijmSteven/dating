df670e398ebaa6026f27a9824a3e5e851ee12c76
apmavrid <apmavrid@gmail.com>
2020-09-25 12:56:13 +0300

    canonical url to lp

--- resources/views/frontend/landing-pages/ads/1.blade.php
+++ resources/views/frontend/landing-pages/ads/1.blade.php
@@ -8,6 +8,7 @@
 
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
     <meta name="viewport" content="width=device-width"/>
+    <link rel="canonical" href="https://altijdsex.nl/lps/1" />
 
 @if(App::environment('production'))
     <!-- Google Tag Manager -->
